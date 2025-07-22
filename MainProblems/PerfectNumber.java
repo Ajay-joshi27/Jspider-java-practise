60ce2cfef479204507d406068e45419147913fa3
ajay-joshi27 <ajayajoshi143@gmail.com>
2025-07-22 21:59:55 +0530

    perfectnumber added

---
+++ MainProblems/PerfectNumber.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class PerfectNumber{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        
+      int n =sc.nextInt();
+      int sum=0;
+      for(int i=1;i<n;i++)
+      {
+          if(n%i==0)
+          {
+              sum+=i;
+          }
+      }
+    //   System.out.print(sum);
+        
+        if(sum==n)
+        {
+            System.out.print("it is perfect number");
+        }
+        else{
+            System.out.println("It Is Not A Perfect Number");
+        }
+    }
+}
